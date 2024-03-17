d848615851380d232ca8f04a6939bbbf7a32e071
Natalia Nosowicz <247745@edu.p.lodz.pl>
2024-03-17 15:08:32 +0100

    kod

---
+++ Wzory/Wzory/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Liczenie
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj pierwszą liczbę:");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Podaj drugą liczbę:");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+
+        int suma = a + b;
+        Console.WriteLine("Suma: " + suma);
+
+
+        int roznica = a - b;
+        Console.WriteLine("Różnica: " + roznica);
+
+        Console.ReadLine();
+
+    }
+}
